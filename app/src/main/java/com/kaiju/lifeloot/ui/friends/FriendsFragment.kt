2cc2c2fbef46769d6c9dd6d75d3d8a3c3cba3f77
kowabunga314 <dougfrost314@gmail.com>
2020-08-02 18:43:32 -0500

    ditching RecyclerView for options page

--- app/src/main/java/com/kaiju/lifeloot/ui/friends/FriendsFragment.kt
+++ app/src/main/java/com/kaiju/lifeloot/ui/friends/FriendsFragment.kt
@@ -1,28 +1,26 @@
-package com.kaiju.lifeloot.ui.notifications
+package com.kaiju.lifeloot.ui.friends
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.kaiju.lifeloot.R
 
-class OptionsFragment : Fragment() {
+class FriendsFragment : Fragment() {
 
-    private lateinit var notificationsViewModel: OptionsViewModel
+    private lateinit var homeViewModel: FriendsViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        notificationsViewModel =
-            ViewModelProviders.of(this).get(OptionsViewModel::class.java)
+        homeViewModel =
+            ViewModelProviders.of(this).get(FriendsViewModel::class.java)
 
-        val root = inflater.inflate(R.layout.fragment_options, container, false)
+        val root = inflater.inflate(R.layout.fragment_friends, container, false)
 
         return root
     }
--- app/src/main/java/com/kaiju/lifeloot/ui/quickstart/QuickstartFragment.kt
+++ app/src/main/java/com/kaiju/lifeloot/ui/quickstart/QuickstartFragment.kt
@@ -1,4 +1,4 @@
-package com.kaiju.lifeloot.ui.dashboard
+package com.kaiju.lifeloot.ui.quickstart
 
 import android.content.Intent
 import android.os.Bundle
@@ -6,13 +6,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.kaiju.lifeloot.GameActivity
 import com.kaiju.lifeloot.R
-import com.kaiju.lifeloot.StartupActivity
 
 class QuickstartFragment : Fragment() {
 
